fdc631936a47b9ff3f7e46dfe6aff2598f7c0a2d
Nicolas Astudillo <nastudillo@metrocapital.cl>
2022-05-30 20:34:23 -0400

    Se modificio los archivos en Fix/

--- colors.py
+++ colors.py
@@ -3,6 +3,7 @@ import os
 
 os.system('')
 
+
 def printRed(skk):      return "\033[91m{}\033[00m".format(skk)
 def printGreen(skk):    return "\033[92m{}\033[00m".format(skk)
 def printYellow(skk):   return "\033[93m{}\033[00m".format(skk)
