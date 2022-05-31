2fde72987dc9345202cdf7350f7a3ae80ad44bda
Nicolas Astudillo <nastudillo@metrocapital.cl>
2022-05-30 20:11:16 -0400

    First commit

---
+++ colors.py
@@ -0,0 +1,14 @@
+
+import os
+
+os.system('')
+
+def printRed(skk):      return "\033[91m{}\033[00m".format(skk)
+def printGreen(skk):    return "\033[92m{}\033[00m".format(skk)
+def printYellow(skk):   return "\033[93m{}\033[00m".format(skk)
+def printBlue(skk):     return "\033[94m{}\033[00m".format(skk)
+def printPink(skk):     return "\033[95m{}\033[00m".format(skk)
+def printCyan(skk):     return "\033[96m{}\033[00m".format(skk)
+def printWhite(skk):    return "\033[97m{}\033[00m".format(skk)
+def printCViolet(skk):  return "\033[35m{}\033[00m".format(skk)
+def printCBeige(skk):   return "\033[36m{}\033[00m".format(skk)
